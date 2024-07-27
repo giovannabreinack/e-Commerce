c83e275ab886db6970f7bccc2693b91ba97c9075
gibreinack <giovannabreinackcolombara@gmail.com>
2024-07-27 20:57:29 -0300

    Inclusão das rotas Post e Delete e do Banco de Dados

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # Importação das bibliotecas
-
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,4 +13,26 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     #O Text ao invés do String não restringe a quantidade de caracteres
     description = db.Column(db.Text, nullable=True)
-    #Definindo uma rota raíz (página inicial) e a função que será executado ao requisitar
+    
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+
+    data = request.json
+
+
+    if 'name' in data and 'price' in data:
+     product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+     db.session.add(product)
+     db.session.commit()
+     return jsonify({"message": "Produto adicionado com sucesso!"})
+    return jsonify({"message": "Dados do produto inválidos"}), 400
+
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product != None:
+       db.session.delete(product)
+       db.session.commit()
+       return jsonify({"message": "Produto deletado com sucesso!"})
+    return jsonify({"message": "Produto não encontrado"}), 404
